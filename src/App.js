9d8cd379d11cf02a859da941439761bd77e143c8
Anastasiia <107393034+Anastasiia345783877@users.noreply.github.com>
2022-08-05 11:01:36 +0300

    first part of visual

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import FComponent from './FComponent';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import BodyComponent from './Body/BodyComponent';
+import NavBarComponent from './NavBarComponent';
 function App() {
 
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         <FComponent firstName = "Anastasiia" lastName = "Pashko" />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Container>
+<NavBarComponent />
+<BodyComponent/>
+  </Container>
   );
 }
 
--- src/FComponent.js
+++ src/FComponent.js
@@ -1,13 +1,23 @@
 import { useState } from "react";
 
-const FComponent = (props) =>{
-//useState eto tak nazyvaemuy hook  koto
-const [count, setCount] = useState(0);
+const FComponent = (props) => {
+     //useState eto tak nazyvaemuy hook kotoryj dajot nam vozmoznost upravliat sostajaniem komponenta
+     //useState funcja prinemaet 1 argument kotoryj naznachaetsa kak znachenije po umalchaniju.
+     //useState vozvraschajet massiv index 0 = znachenije po umolchaniju, index 1 = funcja kotoraja vozdejsvujet na peremennuju
+     //funcja useState = setCount zastavljajet komponent perezapustitsa.
+     const [count, setCount] = useState(0);
 
-return(<div>
-     Vsem privet {props.firstName} {props.lastName}
-<button onClick={() => setCount(count+1) }> Add{count}</button>
-</div>);
-}
+     return (
+          <div>
+               {count ? (<>Vsem privet {props.firstName} {props.lastName}</>) : ''}
+               <button onClick={() => setCount(count + 1)}> Add{count}</button>
+          </div>
+     );
+} 
 
-export default FComponent;
+FComponent.defoultProps = {
+     firstName:"Default Name",
+     lastName: "Default Last Name"
+}; 
+
+     export default FComponent;
